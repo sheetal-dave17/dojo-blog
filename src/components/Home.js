711aba45b3bd3066214eba2e1f17d60557f453d7
sheetal_dave <sheetal@designs.codes>
2021-03-19 13:26:40 +0530

    created custom and reusable useFetch hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,16 @@
 import { useState, useEffect } from 'react';
+import useFetch from '../CustomHooks/useFetch';
 import BlogList from './BlogList';
 
 const Home = () => {
-    
-    const [blogs, setBlogs] = useState(null);
-    const [isLoading, setIsLoading] = useState(true);
-    const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then((res) => {
-                if (!res.ok) {
-                  throw Error('Could not fetch data, something went wrong!!')
-                }
-                return res.json()
-            })
-            .then(data => {
-                setBlogs(data)
-                setIsLoading(false)
-                setError(null);
-            }).catch(error => {
-                setIsLoading(false);
-                setError(error.message);                
-            })
-    }, []);
+    const { data, isLoading, error } = useFetch('http://localhost:8000/blogs');
 
     return (
         <div className="home">
-            { error && <div> {error} </div> }
+            { error && <div> {error} </div>}
             { isLoading && <div> Loading .... </div>}
-            { blogs && <BlogList blogs={blogs} title={'All Blogs'} />}
+            { data && <BlogList blogs={data} title={'All Blogs'} />}
         </div>
     );
 }
